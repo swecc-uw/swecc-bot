30e92d8766e763b14c81a114e7f16c78571f102f
Elijah Melton <98496503+elimelt@users.noreply.github.com>
2024-11-23 20:09:04 +0000

    leaderboard for leetcode (#4)
    * leaderboard for leetcode
    
    * factored out headers
    
    * remove print and add explicit default params

--- APIs/SweccAPI.py
+++ APIs/SweccAPI.py
@@ -6,13 +6,13 @@ class SweccAPI:
     def __init__(self):
         self.url = os.getenv('SWECC_URL')
         self.api_key = os.getenv('SWECC_API_KEY')
-    
-    def auth(self, discord_username, id, username):
-        logging.info(f"Authenticating {discord_username} with id {id} and username {username}")
-        headers = {
+        self.headers = {
             "Authorization": f"Api-Key {self.api_key}",
             "Content-Type": "application/json",
         }
+    
+    def auth(self, discord_username, id, username):
+        logging.info(f"Authenticating {discord_username} with id {id} and username {username}")
 
         data = {
             "discord_id": id,
@@ -20,5 +20,19 @@ class SweccAPI:
             "username": username
         }
 
-        response = requests.put(f"{self.url}/members/verify-discord/", headers=headers, json=data)
+        response = requests.put(f"{self.url}/members/verify-discord/", headers=self.headers, json=data)
         return response.status_code
+
+    def leetcode_leaderboard(self, order_by="total"):
+        logging.info("Fetching leetcode leaderboard order by %s", order_by)
+
+        params = {"order_by": order_by }
+
+        response = requests.get(
+            f"{self.url}/leaderboard/leetcode/", headers=self.headers, params=params
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
