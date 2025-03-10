57343a8b63685dde702dcdbc2a5b4b1697f022d5
Advay Patil <advaypatil27@gmail.com>
2025-03-09 22:02:46 -0700

    Give Gemini context about the current conversation

--- APIs/GeminiAPI.py
+++ APIs/GeminiAPI.py
@@ -1,4 +1,3 @@
- 
 import os, logging
 from google import genai
 from google.genai import types
@@ -7,15 +6,18 @@ logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
 )
 
+
 class GeminiAPI:
 
-    def __init__(self):
+    def __init__(self, max_context_length=1000):
         self.api_key = os.getenv("GEMINI_API_KEY")
-        self.model_name = 'gemini-2.0-flash-001'
+        self.model_name = "gemini-2.0-flash-001"
         self.config = types.GenerateContentConfig(
-            system_instruction='''
+            system_instruction="""
             You are a butler for the Software Engineering Career Club at the University of Washington.
             Keep all of your responses below 200 words.
+
+            You will be given context regarding the conversation. Use the context to respond to the prompt appropriately.
 
             All of your messages will be formatted as follows:
 
@@ -32,22 +34,43 @@ class GeminiAPI:
 
             IMPORTANT: only output your response to the message. You do not need to include who the Author is,
             or any "Message:" prefix. You should only output your response to the message.
-            ''',
+            """,
             max_output_tokens=200,
             temperature=0.8,
         )
         self.prompt = "Gemini"
         self.client = genai.Client(api_key=self.api_key)
+        self.context = []
+        self.context_length = 0
+        self.MAX_CONTEXT_LENGTH = max_context_length
 
     async def prompt_model(self, text):
         try:
             response = await self.client.aio.models.generate_content(
-                model = self.model_name, contents=text, config=self.config
+                model=self.model_name, contents=text, config=self.config
             )
 
             return response.text
         except Exception as e:
             logging.error(f"Error in prompt_model: {e}")
+
+    def update_context(self, message):
+        while (
+            len(self.context) > 0
+            and self.context_length + len(message) >= self.MAX_CONTEXT_LENGTH
+        ):
+            self.context_length -= len(self.context.pop(0))
+        self.context.append(message)
+        self.context_length += len(message)
+        logging.info(f"Context updated: {self.context}")
+
+    def add_context(self, message):
+        return (
+            "-----CONTEXT----\n"
+            + "\n".join(self.context)
+            + "\n----END CONTEXT----"
+            + message
+        )
 
     async def process_message_event(self, message):
         if message.author.bot or not self.prompt.lower() in message.content.lower():
@@ -58,9 +81,17 @@ class GeminiAPI:
         """
 
         logging.info(f"Prompt by user {message.author}: {prompt}")
-        response = await self.prompt_model(prompt)
+
+        contextualized_prompt = self.add_context(prompt)
+
+        logging.info(f"Contextualized prompt: {contextualized_prompt}")
+
+        response = await self.prompt_model(contextualized_prompt)
+
+        self.update_context(f"Prompt: {prompt}Gemini's Response: {response}")
+
         logging.info(f"Response: {response}")
         if len(response) > 4000:
             response = response[:4000] + "..."
-        await message.channel.send(response)
 
+        await message.channel.send(response)
